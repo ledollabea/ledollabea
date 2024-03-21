bc4528b3d806bd3ef6965b356ddcd565fad05217
ledollabea <beatrizramossouza@gmail.com>
2024-03-21 16:03:32 -0300

    :construction: Side navbar in progress

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,1 +1,10 @@
-let nav;
+function openNav() {
+  document.getElementById("mySidenav").style.width = "15.625em";
+  document.getElementById("mySidenav").style.marginLeft = "3.5em";
+  // document.getElementById("main").style.marginLeft = "15.625em";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
